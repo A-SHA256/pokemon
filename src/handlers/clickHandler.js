5b9d92a0abbd27d509ad0c058d7dbfe7739db781
A-SHA256 <arsanshataev0496@gmail.com>
2025-04-16 11:46:36 +0200

    v2

--- src/handlers/clickHandler.js
+++ src/handlers/clickHandler.js
@@ -5,22 +5,30 @@ import { getData } from "../api.js";
 
 const clickHandler = async () => {
     const id = document.getElementById('input');
-    const res = await getData(id.value);
-    const ifExist = document.getElementById('poke-box');
-    if (ifExist) {
-      ifExist.remove();
-    }
-    if (!(res.id in data.chars)) {
-      data.chars[res.id] = res;
-      dom.box[res.id] = pokeBox(
-        data.chars[res.id].name,
-        data.chars[res.id].sprites['front_default'],
-        data.chars[res.id].abilities,
-        data.chars[res.id].stats
-      );
-      document.body.append(dom.box[res.id]);
+    console.log(typeof id.value);
+    if (isNaN(Number(id.value)) || Number(id.value) === 0) {
+      const warning = document.createElement('h1');
+      warning.setAttribute('id', 'poke-box');
+      warning.innerText = 'Please enter a number';
+      document.body.append(warning);
     } else {
+      const res = await getData(id.value);
+      const ifExist = document.getElementById('poke-box');
+      if (ifExist) {
+        ifExist.remove();
+      }
+      if (!(res.id in data.chars)) {
+        data.chars[res.id] = res;
+        dom.box[res.id] = pokeBox(
+          data.chars[res.id].name,
+          data.chars[res.id].sprites['front_default'],
+          data.chars[res.id].abilities,
+          data.chars[res.id].stats
+        );
         document.body.append(dom.box[res.id]);
+      } else {
+          document.body.append(dom.box[res.id]);
+      }
     }
   };
 
